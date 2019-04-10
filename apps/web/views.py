6d46b11a1542e2d1c1c4ca2d5048515449bfa982
Luiz Carlos <lcpojr@gmail.com>
2019-04-10 17:00:49 -0300

    feat: add basic home page

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,2 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import View
+
+from apps.core.models.event import Event
+from apps.core.models.news import News
+
+
+class Home(View):
+    """
+    Its the Home Page view.
+    It should contain the last events and news to be render on website.
+    """
+
+    def get(self, request):
+        # Getting objects
+        events = Event.objects.all().order_by("starts_at")[:8]
+        news = News.objects.all().order_by("publish_date")[:9]
+
+        # Creating context
+        context = {"events": events, "news": news}
+
+        return render(request, "home.html", context)
