2e384ea99632a806e50517c2305aa9562aac150b
Luiz Carlos <lcpojr@gmail.com>
2019-04-14 15:23:53 -0300

    feat: add news and events pages (#39)

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from .views import Home
+from .views import Events, Home, News
 
 app_name = "web"
 
-urlpatterns = [path("", Home.as_view(), name="home")]
+urlpatterns = [
+    path("", Home.as_view(), name="home"),
+    # Events uris
+    path("eventos", Events.as_view(), name="events"),
+    path("eventos/<slug:slug>/", Events.as_view(), name="events-show"),
+    # News uris
+    path("noticias", News.as_view(), name="news"),
+    path("noticias/<slug:slug>/", News.as_view(), name="news-show"),
+]
