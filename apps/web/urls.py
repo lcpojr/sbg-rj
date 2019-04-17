76990a8b490a4be6dcd6f0b1b59ddf7f2fb3fbd2
Pedro Cecchetti <pedro.cecchetti@gmail.com>
2019-04-17 15:40:29 -0300

    feat: add about page

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import Contact, Events, Gallery, Home, News, Products
+from .views import About, Contact, Events, Gallery, Home, News, Products
 
 app_name = "web"
 
@@ -20,4 +20,6 @@ urlpatterns = [
     # Gallery uris
     path("galeria", Gallery.as_view(), name="gallery"),
     path("galeria/<slug:slug>/", Gallery.as_view(), name="gallery-show"),
+    # About uris
+    path("sobre", About.as_view(), name="about")
 ]
